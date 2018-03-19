6f7d58c8e0f73305a508af87239da23018523813
Kim Haapamäki <kimhaa@Kims-MacBook-Air.local>
2018-03-19 12:55:16 +0100

    added simple method to read file

--- matching/src/main.cpp
+++ matching/src/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main()
-{
-    cout << "Hello, World!";
-    return 0;
+// int main()
+// {
+//     cout << "Hello, World!";
+//     return 0;
+// }
+
+// int main () {
+//   ofstream myfile;
+//   myfile.open ("example.txt");
+//   myfile << "Writing this to a file.\n";
+//   myfile.close();
+//   return 0;
+// }
+
+// reading a text file
+
+int main () {
+  string line;
+  ifstream myfile ("../data/sm-bbt-in.txt");
+  if (myfile.is_open())
+  {
+    while ( getline (myfile,line) )
+    {
+      cout << line << '\n';
+    }
+    myfile.close();
+  }
+
+  else cout << "Unable to open file";
+
+  return 0;
 }
