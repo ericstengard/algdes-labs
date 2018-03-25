337e5aa9888f9ea5f48ddcd28ea2a428761a86dd
ericstengard <eric199610@gmail.com>
2018-03-25 20:43:31 +0200

    WIP new solution

--- matching/src/stablemarriage.cc
+++ matching/src/stablemarriage.cc
@@ -7,9 +7,8 @@
 using namespace std;
 string line;
 bool first = true;
-int n;
 bool odd = true;
-int at = 0;
+
 std::vector<string> namesM;
 std::vector<string> namesF;
 std::vector<std::vector<int> > prefM;
@@ -17,75 +16,93 @@ std::vector<std::vector<int> > prefF;
 std::vector<int> couples;
 std::vector<int> queue;
 std::vector<int> trial;
+std::vector<string> test;
+int n;
 void readFile();
 void printData();
 void solve();
 void print();
 int main (int argc, char* argv[]) {
   ios::sync_with_stdio(0);
-  cin.tie(0);
+  //cin.tie(0);
   readFile();
   solve();
   print();
 }
 void readFile() {
-  string line;
+  std::string line;
+  //std::vector<std::string> test;
   getline(cin, line);
-  while ( line.find("#") != std::string::npos ){
+  std::stringstream iss(line);
+  string b;
+  iss >> b;
+  while (b.at(0) == '#'){
     getline(cin, line);
+    std::stringstream iss(line);
+    iss >> b;
   }
-  line.replace( 0, 2, "" );
-  n = atoi( line.c_str() );
-  int temp = 0;
-  while ( temp < n*2){
-    line = "";
-    getline(cin, line);
-    int number;
-    string name;
-    std::stringstream iss( line );
-    if ( iss >> number ){
-      if (iss >> name){
-        if (number % 2 != 0){
-          namesM.push_back(name);
-        } else {
-          namesF.push_back(name);
+  n = atoi(b.erase(0, 2).c_str());
+  int count = 0;
+  while (getline(std::cin, line) && count < n * 4) {
+      try{
+    std::stringstream iss(line);
+    string a;
+    iss >> a;
+    if(a.at(0) != '#' && !(a.size() > 0)){
+      test.push_back(a);
+    }
+  } catch (int e) {
+    cout << "LOL";
+  }
+    count++;
+  }
+  //n = atoi(test.at(0).erase(0, 2).c_str());
+  test.erase(test.begin());
+  int at = 0;
+  bool G = false;
+  for (string lin : test){
+    if (at < n*2){
+      int number;
+      string name;
+      std::stringstream iss( lin );
+      if ( iss >> number ){
+        if (iss >> name){
+          if (number % 2 != 0){
+            namesM.push_back(name);
+          } else {
+            namesF.push_back(name);
+          }
         }
       }
     }
-    temp++;
-  }
-  getline(cin, line);
-  temp = 0;
-  while ( temp < n*2 ){
-    getline(cin, line);
-    line.erase(std::remove(line.begin(), line.end(), ':'), line.end());
-    int number;
-    std::vector<int> prefs (n, -1);
-    std::stringstream iss( line );
-    bool F = true;
-    bool G = true;
-    int i = 1;
-    while ( iss >> number ){
-      if ( F ){
-        F = !F;
-        if (number % 2 != 0){
-          G = true;
-        } else { G = false; }
+    if (n*2 < at && at < n*4) {
+      int number;
+      std::vector<int> prefs (n, -1);
+      std::stringstream iss( lin );
+      string hane;
+      iss >> hane;
+      bool F = true;
+      int i = 1;
+      while ( iss >> number ){
+        if ( F ){
+          F = !F;
+          G = !G;
+        } else {
+          if ( G ){
+            prefs.at( i - 1 ) = number / 2 - 1;
+          } else {
+            prefs.at( number/2 ) = i;
+          }
+          i++;
+        }
+      }
+      if ( G ){
+        prefM.push_back(prefs);
       } else {
-        if ( G ){
-          prefs.at( i - 1 ) = number / 2 - 1;
-        } else {
-          prefs.at( number/2 ) = i;
-        }
-        i++;
+        prefF.push_back(prefs);
       }
     }
-    if ( G ){
-      prefM.push_back(prefs);
-    } else {
-      prefF.push_back(prefs);
-    }
-    temp++;
+    at++;
   }
 }
 void solve() {
@@ -114,9 +131,13 @@ void solve() {
 			queue.push_back(prev);
 		}
   }
+  // sort after males instead of females...
+  for ( unsigned int i = 0; i < couples.size(); i++ ){
+    trial.at(couples.at(i)) = i;
+  }
 }
 void print() {
   for (int i = 0; i < n; i++){
-		std::cout << namesM.at(couples.at(i)) << " -- " << namesF.at(i) << '\n';
+		std::cout << namesM.at(i) << " -- " << namesF.at(trial.at(i)) << '\n';
 	}
 }
