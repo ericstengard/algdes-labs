14eb736a501cf4d02e345a2482f1a2ea99c24a96
kimhaafi <kimhaafi@gmai.com>
2018-03-29 09:22:58 +0200

    Working word-ladder(not optimized) 4.3 stablemarriage

--- matching/src/stablemarriage.cc
+++ matching/src/stablemarriage.cc
@@ -3,22 +3,12 @@
 #include <string>
 #include <list>
 #include <vector>
+#include <stdlib.h>
+#include <stdio.h>
 using namespace std;
-std::vector<string> namesM;
-std::vector<string> namesF;
-std::vector<std::vector<int> > prefsM;
-std::vector<std::vector<int> > prefsF;
-std::vector<int> couples;
-std::vector<int> maletaken;
-std::vector<int> trial;
-std::list<int> queue;
-int nb = 0;
-int mc = 0;
-int fc = 0;
-int n;
-int at = 0;
-int main (){
-  // int n;
+int main (int argc, char const *argv[]){
+  // const clock_t begin1 = clock();
+  int n;
   ios::sync_with_stdio(0);
   cin.tie(0);
   //take out N
@@ -27,20 +17,21 @@ int main (){
   while (line.at(0) == '#'){
     getline(std::cin, line);
   }
-  n = std::atoi(line.erase(0, 2).c_str());
+  line.erase(0, 2);
+  std::stringstream iss( line );
+  iss >> n;
   //read all lines and store them
-  // int at = 0;
-  // std::vector<string> namesM(n,"");
-  // std::vector<string> namesF(n,"");
-  // std::vector<std::vector<int> > prefsM(n, std::vector<int>(n, 0));
-  // std::vector<std::vector<int> > prefsF(n, std::vector<int>(n, 0));
-  // std::vector<string> namesM;
-  // std::vector<string> namesF;
-  // std::vector<std::vector<int> > prefsM;
-  // std::vector<std::vector<int> > prefsF;
-  // int nb = 0;
-  // int mc = 0;
-  // int fc = 0;
+  int at = 0;
+  // const clock_t begin4 = clock();
+  std::vector<string> namesM(n,"");
+  std::vector<string> namesF(n,"");
+  std::vector<std::vector<int> > prefsM(n, std::vector<int>(n, 0));
+  std::vector<std::vector<int> > prefsF(n, std::vector<int>(n, 0));
+  // const clock_t end4 = clock();
+  int nb = 0;
+  int mc = 0;
+  int fc = 0;
+  // const clock_t begin2 = clock();
   while (getline(std::cin, line)){
     if(line.size() > 0 && line.at(0) != '#'){
       if (at < n*2){
@@ -50,24 +41,22 @@ int main (){
         iss >> number;
         iss >> name;
         if (number%2 == 1){
-          // namesM.at(nb) = name;
-          namesM.push_back(name);
+          namesM.at(nb) = name;
         } else {
-          // namesF.at(nb) = name;
-          namesF.push_back(name);
+          namesF.at(nb) = name;
           nb++;
         }
       } else {
         int number;
         string name;
-        // std::vector<int> prefs(n, 0);
-        std::vector<int> prefs;
-        for ( int j = 0; j < n; j++){
-          prefs.push_back(0);
-        }
+        std::vector<int> prefs(n, 0);
         std::stringstream iss( line );
         iss >> name;
-        int num = std::stoi(name, nullptr, 0);
+        name.erase(name.size()-1, name.size());
+        // int num = atoi(name.c_str());
+        int num;
+        std::stringstream tss( name );
+        tss >> num;
         bool M;
         if (num%2 == 1){
           M = true;
@@ -84,32 +73,24 @@ int main (){
           nb++;
         }
         if (M) {
-          // prefsM.at(mc) = prefs;
-          prefsM.push_back(prefs);
+          prefsM.at(mc) = prefs;
           mc++;
         } else {
-          // prefsF.at(fc) = prefs;
-          prefsF.push_back(prefs);
+          prefsF.at(fc) = prefs;
           fc++;
         }
       }
       at++;
     }
   }
+  // const clock_t end2 = clock();
   //algo for perfect match
-  // std::list<int> queue;
-  // std::vector<int> couples(n, 0);
-  // std::vector<int> maletaken(n, 0);
-  // std::vector<int> trial(n, 0);
-  // std::vector<int> couples;
-  // std::vector<int> maletaken;
-  // std::vector<int> trial;
+  // const clock_t begin3 = clock();
+  std::list<int> queue;
+  std::vector<int> couples(n, -1);
+  std::vector<int> maletaken(n, 0);
   for (int i = 0; i < n; i++){
     queue.push_front(i);
-    // couples.at(i) = -1;
-    couples.push_back(-1);
-    maletaken.push_back(0);
-    trial.push_back(0);
   }
   while (queue.size() > 0){
     int male = queue.front();
@@ -131,23 +112,14 @@ int main (){
 			queue.push_front(prev);
 		}
   }
-  //sort them aaccording to males
-  for ( int i = 0; i < n; i++ ){
-    trial.at(couples.at(i)) = i;
-  }
-  //print to console
+  // const clock_t end3 = clock();
   for (int i = 0; i < n; i++){
-		std::cout << namesM.at(i) << " -- " << namesF.at(trial.at(i)) << '\n';
+		std::cout << namesM.at(couples.at(i)) << " -- " << namesF.at(i) << '\n';
 	}
-  // for (int i = 0; i < n; i++){
-	// 	std::cout << namesM[couples[i]] << " -- " << namesF[i] << '\n';
-	// }
-  namesM.clear();
-  namesF.clear();
-  prefsM.clear();
-  prefsF.clear();
-  couples.clear();
-  maletaken.clear();
-  trial.clear();
-  queue.clear();
+  // const clock_t end1 = clock();
+  // std::cout << "WHOLE: " << (end1-begin1)/double(CLOCKS_PER_SEC) << '\n';
+  // std::cout << "READ: " << (end2-begin2)/double(CLOCKS_PER_SEC) << '\n';
+  // std::cout << "ALGO: " << (end3-begin3)/double(CLOCKS_PER_SEC) << '\n';
+  // std::cout << "WRITEDATAS: " << (end4-begin4)/double(CLOCKS_PER_SEC) << '\n';
+  return 0;
 }
---
+++ word-ladders/src/word-ladder.cc
@@ -0,0 +1,88 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <list>
+#include <vector>
+#include <stdlib.h>
+#include <stdio.h>
+#include <set>
+#include <cstddef>
+using namespace std;
+class Node{
+  public:
+    string word;
+    int numSteps;
+  Node(string pword, int pnumSteps){
+    word = pword;
+    numSteps = pnumSteps;
+  }
+};
+int findPath(set<string> words, string from, string to){
+  list<Node> queue;
+  list<string> wordsToErase;
+  queue.push_front(Node(from, 0));
+  while (!queue.empty()){
+    Node top = queue.front();
+    words.erase(top.word);
+    queue.pop_front();
+    string topword = top.word;
+    if (topword.compare(to) == 0){
+      return top.numSteps;
+    }
+    topword.erase(0,1);
+    for (auto newWord : words){
+      string temp = newWord;
+      bool check = true;
+      for(int i = 0; i < 4; i++){
+        std::size_t found = temp.find_first_of(topword.at(i));
+        if (found == std::string::npos){
+          check = false;
+          break;
+        }
+        temp[found] = '\0';
+      }
+      if (check){
+        if (newWord.compare(to) == 0){
+          return top.numSteps+1;
+        }
+        queue.push_back(Node(newWord, top.numSteps+1));
+        wordsToErase.push_front(newWord);
+      }
+    }
+    for (auto w : wordsToErase){
+      words.erase(w);
+    }
+  }
+  return -1;
+}
+int main(int argc, char *argv[]) {
+  const clock_t begin1 = clock();
+  pair<string, string> wordpair;
+  set<string> words;
+  list<pair<string,string> > pairs;
+  char str1 [6];
+  char str2 [6];
+  FILE * pFile;
+  pFile = fopen (argv[1],"r");
+  while (fscanf (pFile, "%s", str1) != EOF){
+    words.insert(str1);
+  }
+  fclose(pFile);
+  pFile = fopen (argv[2],"r");
+  while (fscanf (pFile, "%s %s", str1, str2) != EOF){
+    wordpair = make_pair(str1,str2);
+    pairs.push_back(wordpair);
+  }
+  fclose(pFile);
+  int i = 0;
+  for (auto p : pairs){
+    std::cout << p.first << " to " << p.second << ". " << i << '\n';
+    int temp = findPath(words, p.first, p.second);
+    std::cout << temp << '\n';
+    i++;
+  }
+  const clock_t end1 = clock();
+  std::cout << "WHOLE: " << (end1-begin1)/double(CLOCKS_PER_SEC) << '\n';
+  return 0;
+}
